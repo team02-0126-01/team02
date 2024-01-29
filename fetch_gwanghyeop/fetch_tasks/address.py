7689551c9099356aab0791d46a188c0ffc3b9dfe
NoviceKim <k7679568@naver.com>
2024-01-29 09:32:07 +0900

    class files updated

--- fetch_gwanghyeop/fetch_tasks/address.py
+++ fetch_gwanghyeop/fetch_tasks/address.py
@@ -1,7 +1,10 @@
+from geo import Geo
+
 class Address:
-    def __init__(self, id: int, street: str, suite: str, city: str, zipcode: str):
+    def __init__(self, id: int, street: str, suite: str, city: str, zipcode: str, geo: Geo):
         self.id = id
         self.street = street
         self.suite = suite
         self.city = city
         self.zipcode = zipcode
+        self.geo = geo
--- fetch_gwanghyeop/fetch_tasks/users.py
+++ fetch_gwanghyeop/fetch_tasks/users.py
@@ -1,8 +1,13 @@
+from address import Address
+from company import Company
+
 class Users:
-    def __init__(self, id: int, name: str, username: str, email: str, phone: str, website: str):
+    def __init__(self, id: int, name: str, username: str, address: Address, email: str, phone: str, website: str, company: Company):
         self.id = id
         self.name = name
         self.username = username
+        self.address = address
         self.email = email
         self.phone = phone
         self.website = website
+        self.company = company
