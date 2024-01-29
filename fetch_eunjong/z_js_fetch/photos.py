77fefd7fd5ae69bb1f48f9a1fdca06caae76e92e
jonge5 <jonge54@gmail.com>
2024-01-29 10:02:25 +0900

    team02 0128-2200-1

--- fetch_eunjong/z_js_fetch/photos.py
+++ fetch_eunjong/z_js_fetch/photos.py
@@ -1,8 +1,8 @@
 class Photos:
-    def __init__(self, albumId: int, id: int, title: str, url: str, thumbnailUrl: str):
-        self.albumId = albumId
+    def __init__(self, album_id: int, id: int, title: str, url: str, thumbnail_url: str):
+        self.album_id = album_id
         self.id = id
         self.title = title
         self.url = url
-        self.thumbnailUrl = thumbnailUrl
+        self.thumbnail_url = thumbnail_url
 
